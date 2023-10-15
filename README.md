6f3561bf8f96d6e404e286cb178c34229ff9c6c0
Russel Marcelo <russelmarcelo1509@gmail.com>
2023-10-15 04:05:22 +0200

    uebung01

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# se1
+<h2 style="text-align: center"> Software Engineering 1 WS 2023 / 2024 </h2>
+
+---
+
+#### Author: Russel Marcelo / rmarce2s
+
+In diesem Repository finden Sie meine Ergbnisse für die Übungen. In Zukunft werde
+ich meine Ergebnisse für weitere Übungen ebenfalls in diesem Repository veröffentlichen.
+
+Mit freundlichen Grüßen <br />
+Russel Marcelo
+
+
--- src/uebung01/codesSE2023/src/org/hbrs/se1/ws23/uebung1/control/GermanTranslator.java
+++ src/uebung01/codesSE2023/src/org/hbrs/se1/ws23/uebung1/control/GermanTranslator.java
@@ -1,8 +1,12 @@
 package org.hbrs.se1.ws23.uebung1.control;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GermanTranslator implements Translator {
 
 	public String date = "Okt/2023"; // Default-Wert
+	private static Map<Integer, String> numberToString = new HashMap();
 
 	/**
 	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
@@ -10,7 +14,18 @@ public class GermanTranslator implements Translator {
 	public String translateNumber( int number ) {
 		// [ihr Source Code aus Übung 1-2]
 
-		return "null";
+		numberToString.put(1, "eins");
+		numberToString.put(2, "zwei");
+		numberToString.put(3, "drei");
+		numberToString.put(4, "vier");
+		numberToString.put(5, "fünf");
+		numberToString.put(6, "sechs");
+		numberToString.put(7, "sieben");
+		numberToString.put(8, "acht");
+		numberToString.put(9, "neun");
+		numberToString.put(10, "zehn");
+
+		return numberToString.getOrDefault(number, "Übersetzung der Zahl " + number + " nicht möglich " + this.version);
 	}
 
 	/**
@@ -27,5 +42,4 @@ public class GermanTranslator implements Translator {
 	public void setDate( String date ) {
 		this.date = date;
 	}
-
 }
